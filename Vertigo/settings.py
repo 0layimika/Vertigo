bcb7f128c0176f931f47516aecfca906a69cc4e0
0layimika <110624406+0layimika@users.noreply.github.com>
2023-09-08 13:20:53 +0100

    changes

--- Vertigo/settings.py
+++ Vertigo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*9%zr#+f^8pjhuqiv((yinr_j2b$e9n+nq_xkr%(oy8jhkuw%7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','wearvertigo.onrender.com']
 
 
 # Application definition
